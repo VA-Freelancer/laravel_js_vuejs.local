c1833441f47d8feedafe6a53d864788f002b393e
VA-Freelancer <vproger@vk.com>
2022-12-24 23:41:38 +0400

    add: vue method add field changed ad add html fields

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -8,6 +8,7 @@
                         <th scope="col">Name</th>
                         <th scope="col">Age</th>
                         <th scope="col">Job</th>
+                        <th scope="col">Edit</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -17,6 +18,14 @@
                         <td>{{ person.name }}</td>
                         <td>{{ person.age }}</td>
                         <td>{{ person.job }}</td>
+                        <td><a href="#" @click.prevent="changeEditPersonId(person.id)" class="btn btn-success">Edit</a></td>
+                    </tr>
+                    <tr :class="isEdit(person.id) ? '' : 'd-none'">
+                        <th scope="row">{{ person.id }}</th>
+                        <th scope="row"> <input type="text" name="" id="" class="form-control"> </th>
+                        <th scope="row"> <input type="number" name="" id="" class="form-control"> </th>
+                        <th scope="row"> <input type="text" name="" id="" class="form-control"> </th>
+                        <td><a href="#" @click.prevent="changeEditPersonId(null)" class="btn btn-success" >Update</a></td>
                     </tr>
                 </template>
 
@@ -33,7 +42,8 @@ export default {
     name: "IndexComponent",
     data(){
         return {
-            people: null
+            people: null,
+            editPersonId: null
         }
     },
     mounted() {
@@ -45,7 +55,15 @@ export default {
                 .then(res =>{
                     this.people = res.data;
                 })
+        },
+        changeEditPersonId(id){
+            console.log(id);
+            this.editPersonId = id;
+        },
+        isEdit(id){
+            return this.editPersonId === id
         }
+
     }
 }
 </script>
