122f92d67446e6dce600940007b9d644967724ed
VA-Freelancer <vproger@vk.com>
2022-12-25 01:09:16 +0400

    upd: add method index to create rand html list people

---
+++ resources/js/components/SomeComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "SomeComponent",
+    data(){
+        return {
+
+        }
+    },
+    mounted() {
+        console.log(this.$parent.$parent.parentLog(), 'Some')
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style scoped>
+</style>
